c70586641c5beb9b7bf968ac949f4ec133561676
Akshay singh <61555659+akkiitsme@users.noreply.github.com>
2024-04-05 09:01:24 +0530

    Update ApiConfig.java

--- api-gateway/src/main/java/com/apigateway/config/ApiConfig.java
+++ api-gateway/src/main/java/com/apigateway/config/ApiConfig.java
@@ -3,6 +3,7 @@ package com.apigateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 
 @Configuration
 public class ApiConfig {
